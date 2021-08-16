9dddfd757f7d0a43ab2ba903b9e5c86d2b70a227
Yvrauj <89006056+Yvrauj@users.noreply.github.com>
2021-08-16 12:50:04 +0530

    Create strings.cpp

---
+++ STRINGS/strings.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    //terminate with null character '\0'
+
+    /*char temp = cin.get();
+    char ans[100];
+
+    int i = 0;
+
+    while (temp != '#')
+    {
+        ans[i++] = temp;
+
+        temp = cin.get();
+    }
+    ans[i] = '\0';
+    imp
+            cout
+        << ans;
+
+    char ch;
+    ch = cin.get();
+
+    int alpha = 0;
+    int digits = 0;
+    int spaces = 0;
+
+    while (ch != '\n')
+    {
+        if (ch == ' ')
+        {
+            spaces++;
+        }
+        else if (ch >= 'a' and ch <= 'z')
+        {
+            alpha++;
+        }
+        else
+        {
+            digits++;
+        }
+
+        ch = cin.get();
+    }
+
+    cout << alpha << " " << spaces << " " << digits << endl;
+
+    //getline
+
+    char sentence[1000];
+
+    cin.getline(sentence, 1000, '.');
+    stopping character default is nline.'\n'.
+
+        cout
+        << sentence << endl;
+
+        */
+
+    char a[1000] = "apple";
+    char b[1000] = " is overrated";
+
+    cout << strcat(a, b) << endl;
+    char c[100] = "mango";
+
+    cout << strcmp(a, a) << endl;
+    return 0;
+}
