c82f1287218f1a635bb0b13062839714a323f77b
Yvrauj <89006056+Yvrauj@users.noreply.github.com>
2021-09-07 20:38:57 +0530

    Create demo.cpp

---
+++ POINTERS/demo.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+#define fo(i, n) for (int i = 0; i < n; i++)
+#define rep(n, m) for (int i = n; i <= m; i++)
+#define all(v) v.begin(), v.end()
+#define ff first
+#define ss second
+#define pb push_back
+#define mp make_pair
+#define os(st) unordered_set<int> st;
+
+
+using namespace std;
+
+
+int arr[21][21];
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+
+    int i;
+
+    int*p = &i;
+
+    cout << p << endl;
+
+    float f = 10.2;
+    float*pf = &f;
+
+    double d = 12.22;
+    double*df = &d;
+
+    return 0;
+}
